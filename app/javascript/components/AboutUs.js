a6ee203f6965fd1b3627f2c31923352d9ec0f965
Desmond Sadler <desmond17@protonmail.com>
2020-06-11 23:22:21 -0700

    type

---
+++ app/javascript/components/AboutUs.js
@@ -0,0 +1,1 @@
+// https://blog.viktorbezic.com/post/154553724265/special-wrongness
--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -3,155 +3,193 @@ import styled from 'styled-components';
 
 const tracks = [
 	{
-		link: 'bakery_house_band/BHB_Baby_Can_Wait_Mix_v3.mp3',
+		link: 'bakery_house_band/BHB_Baby_Can_Wait_Mix_v3',
+		type: 'mp3',
 		name: 'Baby can wait'
 	},
 	{
-		link: 'bakery_house_band/BHB_Baby_I_Do_Mix_v2.mp3',
+		link: 'bakery_house_band/BHB_Baby_I_Do_Mix_v2',
+		type: 'mp3',
 		name: 'Baby I do'
 	},
 	{
-		link: 'bakery_house_band/BHB_I_Know_Mix_v1.mp3',
+		link: 'bakery_house_band/BHB_I_Know_Mix_v1',
+		type: 'mp3',
 		name: 'I know'
 	},
 	{
-		link: 'bakery_house_band/BHB_Lonely_With_You_Mix_v2.mp3',
+		link: 'bakery_house_band/BHB_Lonely_With_You_Mix_v2',
+		type: 'mp3',
 		name: 'Lonely with you'
 	},
 	{
-		link: 'bakery_house_band/BHB_My_Heart_Will_Go_On_Mix_v2.mp3',
+		link: 'bakery_house_band/BHB_My_Heart_Will_Go_On_Mix_v2',
+		type: 'mp3',
 		name: 'My heart will go on'
 	},
 	{
-		link: 'bakery_house_band/BHB_Siren_Song_Mix_v2.mp3',
+		link: 'bakery_house_band/BHB_Siren_Song_Mix_v2',
+		type: 'mp3',
 		name: 'Siren song'
 	},
 	{
-		link: 'bakery_house_band/BHB_Stranger_Dont_Mind_MIX_v2.mp3',
+		link: 'bakery_house_band/BHB_Stranger_Dont_Mind_MIX_v2',
+		type: 'mp3',
 		name: "Stranger don't mind"
 	},
 	{
-		link: 'bakery_house_band/BHB_All_I_Ever_Wanted_From_You_Mix_v1.mp3',
+		link: 'bakery_house_band/BHB_All_I_Ever_Wanted_From_You_Mix_v1',
+		type: 'mp3',
 		name: 'All I ever wanted from you'
 	},
 	{
-		link: 'bakery_house_band/BHB_Summer_Ecstasy_Mix_v3.mp3',
+		link: 'bakery_house_band/BHB_Summer_Ecstasy_Mix_v3',
+		type: 'mp3',
 		name: 'Summer ecstasy'
 	},
 	{
-		link: 'bakery_house_band/BHB_Talk_To_Me_Mix_v1.mp3',
+		link: 'bakery_house_band/BHB_Talk_To_Me_Mix_v1',
+		type: 'mp3',
 		name: 'Talk to me'
 	},
 	{
-		link: 'take_your_time_girl.m4a',
+		link: 'take_your_time_girl',
+		type: 'm4a',
 		name: 'Take your time girl'
 	},
 	{
-		link: 'beat.mp3',
+		link: 'beat',
+		type: 'mp3',
 		name: 'Beat 10'
 	},
 	{
-		link: '12albert.mp3',
+		link: '12albert',
+		type: 'mp3',
 		name: 'Albert'
 	},
 	{
-		link: '14beat.mp3',
+		link: '14beat',
+		type: 'mp3',
 		name: 'Beat 14'
 	},
 	{
-		link: '15suicide.mp3',
+		link: '15suicide',
+		type: 'mp3',
 		name: 'Suicide'
 	},
 	{
-		link: '16bells.mp3',
+		link: '16bells',
+		type: 'mp3',
 		name: 'Bells'
 	},
 	{
-		link: '20beat.mp3',
+		link: '20beat',
+		type: 'mp3',
 		name: 'Beat 20'
 	},
 	{
-		link: '21beat.mp3',
+		link: '21beat',
+		type: 'mp3',
 		name: 'Beat 21'
 	},
 	{
-		link: '22beat.mp3',
+		link: '22beat',
+		type: 'mp3',
 		name: 'Beat'
 	},
 	{
-		link: '8grown.mp3',
+		link: '8grown',
+		type: 'mp3',
 		name: 'Grown'
 	},
 	{
-		link: '9spider.mp3',
+		link: '9spider',
+		type: 'mp3',
 		name: 'Spider'
 	},
 	{
-		link: 'Cadillac.mp3',
+		link: 'Cadillac',
+		type: 'mp3',
 		name: 'Cadillac'
 	},
 	{
-		link: 'Nature Boy.mp3',
+		link: 'Nature Boy',
+		type: 'mp3',
 		name: 'Nature Boy'
 	},
 	{
-		link: 'Tablasong.mp3',
+		link: 'Tablasong',
+		type: 'mp3',
 		name: 'Tablasong'
 	},
 	{
-		link: 'The Crunch.mp3',
+		link: 'The Crunch',
+		type: 'mp3',
 		name: 'The Crunch'
 	},
 	{
-		link: "You're not alone.mp3",
+		link: "You're not alone",
+		type: 'mp3',
 		name: "You're not alone"
 	},
 	{
-		link: 'bogslore.mp3',
+		link: 'bogslore',
+		type: 'mp3',
 		name: 'Bogslore'
 	},
 	{
-		link: 'cool.mp3',
+		link: 'cool',
+		type: 'mp3',
 		name: 'Cool'
 	},
 	{
-		link: 'dontstop.mp3',
+		link: 'dontstop',
+		type: 'mp3',
 		name: "Don't stop"
 	},
 	{
-		link: 'feeldarydam.mp3',
+		link: 'feeldarydam',
+		type: 'mp3',
 		name: 'Feeldarydam'
 	},
 	{
-		link: 'haircutday.mp3',
+		link: 'haircutday',
+		type: 'mp3',
 		name: 'Haircut day'
 	},
 	{
-		link: 'ligo.mp3',
+		link: 'ligo',
+		type: 'mp3',
 		name: 'Ligo'
 	},
 	{
-		link: 'liveforpeace2.mp3',
+		link: 'liveforpeace2',
+		type: 'mp3',
 		name: 'Live for peace'
 	},
 	{
-		link: 'makeitwithyou.mp3',
+		link: 'makeitwithyou',
+		type: 'mp3',
 		name: 'Make it with you'
 	},
 	{
-		link: 'newbells.mp3',
+		link: 'newbells',
+		type: 'mp3',
 		name: 'New bells'
 	},
 	{
-		link: 'nozo.mp3',
+		link: 'nozo',
+		type: 'mp3',
 		name: 'Nozo'
 	},
 	{
-		link: 'taxidriver.mp3',
+		link: 'taxidriver',
+		type: 'mp3',
 		name: 'Taxi driver'
 	},
 	{
-		link: 'whatmoredoyouwant.mp3',
+		link: 'whatmoredoyouwant',
+		type: 'mp3',
 		name: 'What more do you want'
 	}
 ];
@@ -221,7 +259,7 @@ class Home extends React.Component {
 								{this.showControlsFor(i) && (
 									<audio controls>
 										<source
-											src={`https://beatoftheday.s3-us-west-1.amazonaws.com/${obj.link}`}
+											src={`https://beatoftheday.s3-us-west-1.amazonaws.com/${obj.link}.${obj.type}`}
 											type="audio/mp3"
 										/>
 									</audio>
