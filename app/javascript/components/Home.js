25757d8f91e0f800aa49d8cf3c3c8ab4af14b467
Desmond Sadler <desmond17@protonmail.com>
2020-06-18 23:33:27 -0700

    Move

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Player from './Player';
+import Player from './shared/Player';
 import AddATrack from './AddATrack';
 import TrackShow from './TrackShow';
 import ArtistShow from './ArtistShow';
