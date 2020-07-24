9e5a4a86e7e82777dfade5275674b226fc2d8195
Desmond Sadler <desmond17@protonmail.com>
2020-07-23 19:36:17 -0700

    at-mention email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,8 @@
 class UserMailer < ApplicationMailer
-  def
-
+  def send_at_mention_email
+    mail(
+      to: "team@tunelark.com",
+      subject: "Alert: No future lessons scheduled between #{@customer.full_name} and #{@instructor.full_name}",
+    )
   end
 end
