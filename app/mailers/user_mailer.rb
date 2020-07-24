c3dc0a34913dc9f4b8f08f12543c22af9fd2b8ed
Kyle Reeves <kylefr2@gmail.com>
2020-07-23 20:24:12 -0700

    added rebound email html and text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,17 @@
 class UserMailer < ApplicationMailer
+  default :from => 'Beat of the Day <no-reply@beatoftheday.org>'
+
+  def send_rebound_email(track)
+    @track = track
+    @old_track = @track.rebound_from
+    return if @track.artist.id == @old_track.artist.id
+
+    mail(
+      to: @old_track.artist.email,
+      subject: "Somebody rebounded your track \"#{@old_track.name}\" on Beat of the Day!",
+    )
+  end
+
   def send_at_mention_email
     mail(
       to: "team@tunelark.com",
