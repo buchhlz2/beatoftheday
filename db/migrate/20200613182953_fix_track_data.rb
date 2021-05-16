86b1deea0ebfba4ac76fe57d84808c72abb38dce
Ammad Fareed <ammad.farid@arkhitech.com>
2021-05-16 10:28:36 +0500

    Fixed mimemagic, added ask mailer files and removed unnecessary migrations

--- db/migrate/20200613182953_fix_track_data.rb
+++
@@ -1,8 +0,0 @@
-class FixTrackData < ActiveRecord::Migration[6.0]
-  def change
-    User.first.update!(artist_name: "Desmond")
-    Track.all.each do |track|
-      track.update!(photo: "https://beatoftheday.s3-us-west-1.amazonaws.com/aBF9zzMQ.jpeg")
-    end
-  end
-end
